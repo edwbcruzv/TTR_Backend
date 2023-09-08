095da026987d813e17cf8c365060cf2ff9509797
PsyDuck <david-balta@hotmail.com>
2023-09-08 03:21:38 -0600

    Creacion del backend del proyecto

---
+++ src/main/java/com/escom/Creadordecasos/CreadorDeCasosApplication.java
@@ -0,0 +1,13 @@
+package com.escom.Creadordecasos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreadorDeCasosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreadorDeCasosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/escom/Creadordecasos/CreadorDeCasosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escom.Creadordecasos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreadorDeCasosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
