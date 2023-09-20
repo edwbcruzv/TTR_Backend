96a0b20e0c9ca6e346e711a1110ddce21c8dafdc
cruz <edwin.cruz.v@outlook.com>
2023-09-20 04:53:30 -0600

    Agregando Clase Response by Cruz

--- src/main/java/com/escom/Creadordecasos/Controller/AuthenticationController.java
+++ src/main/java/com/escom/Creadordecasos/Controller/AuthenticationController.java
@@ -3,15 +3,13 @@ package com.escom.Creadordecasos.Controller;
 import com.escom.Creadordecasos.Dto.AuthenticationRequest;
 import com.escom.Creadordecasos.Dto.AuthenticationResponse;
 import com.escom.Creadordecasos.Dto.RegistrationRequest;
+import com.escom.Creadordecasos.Dto.Response;
 import com.escom.Creadordecasos.Service.AuthenticationService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -21,26 +19,24 @@ public class AuthenticationController {
     AuthenticationService authenticationService;
 
     @PreAuthorize("permitAll")
-    @PostMapping(value = "authenticate")
-    public ResponseEntity<AuthenticationResponse> login( @RequestBody @Valid
-                                                         AuthenticationRequest authRequest ) {
-        AuthenticationResponse jwtDto = authenticationService.login( authRequest );
+    @PostMapping(value = "/authenticate")
+    public @ResponseBody Response<AuthenticationResponse> login(@RequestBody @Valid AuthenticationRequest authRequest) {
 
-        return ResponseEntity.ok( jwtDto );
+        return authenticationService.login(authRequest);
     }
 
     @PreAuthorize("permitAll")
-    @PostMapping(value = "register-student")
-    public ResponseEntity<AuthenticationResponse> registerAsStudent( @RequestBody @Valid
-                                                                     RegistrationRequest regRequest ) {
-        AuthenticationResponse jwtDto = authenticationService.registerAsStudent( regRequest );
-        return ResponseEntity.ok( jwtDto );
+    @PostMapping(value = "/register-student")
+    public @ResponseBody Response<AuthenticationResponse> registerAsStudent(@RequestBody @Valid RegistrationRequest regRequest) {
+
+        return authenticationService.registerAsStudent(regRequest);
     }
 
     @PreAuthorize("permitAll")
-    @PostMapping(value = "register-teacher")
-    public ResponseEntity<AuthenticationResponse> registerAsTeacher( @RequestBody @Valid RegistrationRequest regRequest ) {
-        AuthenticationResponse jwtDto = authenticationService.registerAsTeacher( regRequest );
-        return ResponseEntity.ok( jwtDto );
+    @PostMapping(value = "/register-teacher")
+    public @ResponseBody Response<AuthenticationResponse> registerAsTeacher(@RequestBody @Valid RegistrationRequest regRequest) {
+
+        return authenticationService.registerAsTeacher(regRequest);
     }
 }
+
