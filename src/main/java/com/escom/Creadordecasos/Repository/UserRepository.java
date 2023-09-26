81c4e7b13aa86ca29f235ca507851015dc27bcce
cruz <edwin.cruz.v@outlook.com>
2023-09-25 23:34:42 -0600

    Correcciones by cruz

--- src/main/java/com/escom/Creadordecasos/Repository/UserRepository.java
+++ src/main/java/com/escom/Creadordecasos/Repository/UserRepository.java
@@ -11,5 +11,9 @@ import java.util.Optional;
  */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+    Optional<User> findByUsernameIgnoreCase(String username);
+
+    Optional<User> findByEmailIgnoreCase(String email);
+
+
 }
