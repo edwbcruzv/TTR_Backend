666727a614fe4b0c9706f48ba2e1f3cfc0b43e0e
cruz <edwin.cruz.v@outlook.com>
2024-05-04 01:20:25 -0600

    Antes de las practicas y soluciones

---
+++ src/main/java/com/escom/CreadorPracticas/Dto/PracticaDTO.java
@@ -0,0 +1,12 @@
+package com.escom.CreadorPracticas.Dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class PracticaDTO {
+    private Long id;
+}
---
+++ src/main/java/com/escom/CreadorPracticas/Service/Solucion/Bodies/SolucionReq.java
@@ -0,0 +1,14 @@
+package com.escom.CreadorPracticas.Service.Solucion.Bodies;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class SolucionReq {
+    private Long id;
+}
