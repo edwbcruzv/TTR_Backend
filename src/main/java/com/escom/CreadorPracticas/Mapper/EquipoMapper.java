730ce9d3d81e9122fb201eb0f67a69c11610a9f6
cruz <edwin.cruz.v@outlook.com>
2024-05-06 01:10:28 -0600

    Falta Soluciones y practicas

--- src/main/java/com/escom/CreadorPracticas/Mapper/EquipoMapper.java
+++ src/main/java/com/escom/CreadorPracticas/Mapper/EquipoMapper.java
@@ -2,13 +2,28 @@ package com.escom.CreadorPracticas.Mapper;
 
 import com.escom.CreadorPracticas.Dto.EquipoDTO;
 import com.escom.CreadorPracticas.Entity.Equipo;
+import com.escom.CreadorPracticas.Entity.Estudiante;
 import org.mapstruct.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface EquipoMapper {
+    @Mapping(target = "grupoId", source = "grupo.id")
+    @Mapping(target = "estudiantesUsernames", source = "estudiantes")
     EquipoDTO toDto(Equipo entity);
     List<EquipoDTO> toListDto(List<Equipo> list);
     Equipo toEntity(EquipoDTO dto);
+
+    default List<String> mapEstudiantesList(List<Estudiante> estudiantes) {
+        if(estudiantes!=null) {
+            return estudiantes.stream()
+                    .map(Estudiante::getUsername)
+                    .collect(Collectors.toList());
+        }else{
+            return new ArrayList<String>();
+        }
+    }
 }
--- src/main/java/com/escom/CreadorPracticas/Mapper/GrupoMapper.java
+++ src/main/java/com/escom/CreadorPracticas/Mapper/GrupoMapper.java
@@ -1,15 +1,42 @@
 package com.escom.CreadorPracticas.Mapper;
 
 import com.escom.CreadorPracticas.Dto.GrupoDTO;
+import com.escom.CreadorPracticas.Entity.Equipo;
 import com.escom.CreadorPracticas.Entity.Grupo;
+import com.escom.CreadorPracticas.Entity.Inscripcion;
+import com.escom.CreadorPracticas.Entity.Profesor;
 import org.mapstruct.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface GrupoMapper {
+
+    @Mapping(source = "equipos", target = "equiposIds")
+    @Mapping(source = "profesor", target = "profesorNombre")
     GrupoDTO toDto(Grupo entity);
     List<GrupoDTO> toListDto(List<Grupo> list);
     Grupo toEntity(GrupoDTO dto);
+
+    @AfterMapping
+    default void mapInscripcionesToDTO(@MappingTarget GrupoDTO dto, Grupo entity){
+        dto.setProfesorNombre(concatenarNombresProfesor(entity.getProfesor()));
+    }
+    default List<Long> map_equipos(List<Equipo> equipos) {
+
+        if (equipos != null) {
+            return equipos.stream()
+                    .map(Equipo::getId)
+                    .collect(Collectors.toList());
+        }else{
+            return new ArrayList<>();
+        }
+    }
+
+    default String concatenarNombresProfesor(Profesor persona) {
+        return persona.getNombre() + " " + persona.getApellidoPaterno() + " " + persona.getApellidoMaterno();
+    }
 
 }
